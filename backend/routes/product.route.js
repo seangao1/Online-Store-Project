1e65ff0957a5ff88cb9cad6ef109582ffb186052
sean-gao1 <sean.gao1@outlook.com>
2025-04-23 23:49:46 -0400

    feat(product): add controller and routes for GET, POST, PUT, DELETE

---
+++ backend/routes/product.route.js
@@ -0,0 +1,21 @@
+import express from 'express';
+
+
+import { getProducts, createProduct, updateProduct, deleteProduct} from '../controllers/product.controller.js';
+
+const router = express.Router();
+
+// endpoint for etting all products;
+router.get('/', getProducts);
+
+// endpoint for adding(posting) a product
+router.post("/", createProduct);
+
+// endpoint for updating(putting) a product:
+router.put('/:id', updateProduct);
+
+// endpoint for deleting a product:
+router.delete("/:id", deleteProduct);
+
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 
 import {connectDB} from './config/db.js';
 
-import Product from './models/product.model.js';
+import productRoutes from './routes/product.route.js';
 
 
 dotenv.config();
@@ -14,41 +14,11 @@ dotenv.config();
 const app = express();
 
 
+
+
 app.use(express.json()); // This allows the app to parse json data in the req.body;
 
-app.post("/api/products", async (req, res) => {
-    const product = req.body; // user will send this data;
-    if (!product.name || !product.price || !product.image) {
-        return res.status(400).json({success: false, message:"Please provide all fields."});
-    };
-
-    const newProduct = new Product(product);
-
-    try {
-        await newProduct.save();
-        res.status(201).json({success: true, data: newProduct});
-    }
-    catch(error){
-        console.log('error in creating product', error);
-        res.status(501).json({success: false, message:"Server error."})
-    }
-
-
-});
-
-
-app.delete("/api/products/:id", async (req, res) => {
-
-    const {id} = req.params;
-    try {
-        await Product.findByIdAndDelete(id);
-        res.status(200).json({success: true, message: "Product deleted."});
-    }
-    catch(err){
-        
-        res.status(501).json({success: false, message: 'Product not found.'});
-    }
-});
+app.use('/api/products', productRoutes);
 
 
 app.listen(3000, () => {

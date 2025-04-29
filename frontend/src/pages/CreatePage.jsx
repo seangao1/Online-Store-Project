71e8836a90e20f946fbfb263bab3a441ddf5d8c8
sean-gao1 <sean.gao1@outlook.com>
2025-04-28 22:31:31 -0400

    frontend: added input boxes for adding products

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import { useState } from 'react';
+import { Box, Button, Container, Heading, Input, useColorModeValue, VStack } from '@chakra-ui/react';
+
 
 const CreatePage = () => {
-  return (
-    <div>CreatePage</div>
-  )
+
+    const [newProduct, setNewProduct] = useState({
+        name: '',
+        price: '',
+        image: '',
+    });
+
+    const handleAddProduct = () => {
+        console.log(newProduct);
+    };
+
+
+    return (
+        <Container maxW={"container.sm"}>
+            <VStack spacing={8}>
+                <Heading as={'h1'} size={"2xl"} textAlign={"center"} mb={6}>
+                    Create New Product
+                </Heading>
+
+                <Box
+                    w={"full"} bg={useColorModeValue("white", "grey.800")} p={6} >
+
+                    <VStack>
+                        <Input
+                            placeholder='Product Name'
+                            name='name'
+                            value={newProduct.name}
+                            onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
+                        />
+
+                        <Input
+                            placeholder='Price'
+                            name='price'
+                            type='number'
+                            value={newProduct.price}
+                            onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })}
+                        />
+
+                        <Input
+                            placeholder='Image URL'
+                            name='image'
+                            value={newProduct.image}
+                            onChange={(e) => setNewProduct({ ...newProduct, image: e.target.value })}
+                        />
+
+                        <Button colorScheme='orange' onClick={handleAddProduct} w='full'>
+                            Add Product
+                        </Button>
+                    </VStack>
+                </Box>
+            </VStack>
+        </Container>
+    )
 }
 
-export default CreatePage
+export default CreatePage;

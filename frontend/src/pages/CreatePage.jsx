3edcfcf282aa17e33563b9d8c04bb2f26cd79a39
sean-gao1 <sean.gao1@outlook.com>
2025-05-01 23:50:34 -0400

    frontend: added Homepage components for displaying products

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Box, Button, Container, Heading, Input, useColorModeValue, VStack } from '@chakra-ui/react';
+import { Box, Button, Container, Heading, Input, useColorModeValue, VStack, useToast } from '@chakra-ui/react';
 import { useProductStore } from '../store/product';
 
 
@@ -11,12 +11,35 @@ const CreatePage = () => {
         image: '',
     });
 
-    const {createProduct} = useProductStore();
+    const toast = useToast();
+
+    const { createProduct } = useProductStore();
 
     const handleAddProduct = async () => {
-        const {success, message} = await createProduct(newProduct);
+        const { success, message } = await createProduct(newProduct);
         console.log('Success:', success);
         console.log('Message:', message);
+
+        
+        if (!success) {
+            toast({
+                title: 'Error',
+                description: message,
+                status: 'error',
+                isClosable: true
+            });
+        }
+        else {
+            toast({
+                title: 'Success',
+                description: message,
+                status: 'success',
+                isClosable: true
+            });
+        }
+
+        setNewProduct({name:'', price:'', image:''});
+
     };
 
 
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { Container, VStack, Text, Link } from "@chakra-ui/react";
+//import {Link} from 'react-router-dom';
 
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
+    return (
+
+        <Container maxW="container.xl" py={12}>
+            <VStack spacing={8}>
+                <Text
+                    fontSize='30px'
+                    fontWeight={'bold'}
+                    bgGradient={"linear(to-br , orange.800, red.300)"}
+                    bgClip={'text'}
+                    textAlign={'center'}
+                >
+                    Current Prodcuts
+                </Text>
+
+                <Text fontSize='xl' textAlign={'center'} fontWeight={'bold'} color='red.600'>
+                    No products found :(     {"  "}
+
+
+                    <Link href="/create">
+                        <Text as='span' color='blue.600' _hover={{ textDecoration: "underline" }}>
+                            Create a product
+                        </Text>
+                    </Link>
+                </Text>
+            </VStack>
+
+        </Container>
+
+
+    );
 }
 
 export default HomePage
